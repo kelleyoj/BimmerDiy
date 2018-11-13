837e19a7454d78be244eb49bd45c0357c7d5b29f
OJ <kelleyoj@gmail.com>
2018-11-13 06:26:40 -0500

    add card flip

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-$("#slideshow > div:gt(0)").hide();
 
-setInterval(function() { 
-  $('#slideshow > div:first')
-    .fadeOut(8000)
-    .next()
-    .fadeIn(8000)
-    .end()
-    .appendTo('#slideshow');
-},  8000);
+
+$(document).ready(function() {
+  $(function($) {
+    $("#card").flip(); 
+  });
+})
